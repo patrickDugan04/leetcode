152bcbfe429e1128832174ae315009a12f1feb24
Patrick Dugan <patrickdugan04@gmail.com>
2020-06-23 20:59:22 -0400

    Did 1221 with the naive solution

---
+++ 1221/1221.py
@@ -0,0 +1,15 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        R = 0
+        L = 0
+        d = 0
+        for i in s:
+            if i == 'R':
+                R+=1
+            else:
+                L+=1
+            if R==L:
+                d+=1
+        return d
+
+        
